608ff7a358bae8fbdbd09c5d8b2aa31658c9055d
Maruf Khan <maruf_kh01@yahoo.com>
2018-02-27 15:34:42 +0100

    ulp

--- src/components/dragdrop.js
+++ src/components/dragdrop.js
@@ -1,16 +1,53 @@
 import React, { Component } from 'react';
 
+import Dropzone from 'react-dropzone';
 
 class Dragdrop extends Component{
-    constructor(){
-        super();
-
+    constructor(props){
+        super(props);
+        this.state={
+            filesPreview:[],
+            filesToBeSent:[],
+            printcount:10,
+        }
+    }
+    onDrop(acceptedFiles) {
+        console.log('Accepted files: ', acceptedFiles[0].name);
+        var filesToBeSent=this.state.filesToBeSent;
+        if(filesToBeSent.length < this.state.printcount){
+            filesToBeSent.push(acceptedFiles);
+            var filesPreview=[];
+            for(var i in filesToBeSent){
+                filesPreview.push(
+                    <div>
+                        {filesToBeSent[i][0].name}
+                    </div>
+                )
+            }
+            this.setState({filesToBeSent,filesPreview});
+        }
+        else{
+            alert("You have reached the limit of printing files at a time")
+        }
     }
     render(){
         return(
-            <div className="">
 
+            <div className="upp_down">
+                <div className="upload_area">
+                        <Dropzone onDrop={(files) => this.onDrop(files)}>
+                            <div>Try dropping some files here, or click to select files to upload.</div>
+                        </Dropzone>
+                </div>
+                <div className="display">
+                Files to be printed are:
+                {this.state.filesPreview}
+                </div>
+                <div className="download_area">
+
+                </div>
             </div>
+
         );
     }
 }
--- src/components/signin.js
+++ src/components/signin.js
@@ -33,41 +33,43 @@ class SignIn extends Component {
     render(){
         return(
             <div className="main_section">
-                <div className="row column text center">
-                    <h1>
-                        <div className="main_section_logo">
-                            <img className="logo1" src={require("../image/logo.png")} alt=""/>
-                            <h2 className="text">Consider design is a toll to get clients feedback.</h2>
-                            <p className="text"> Makes your process fast.</p>
+                <div className="section1">
+                    <div className="row column text center">
+                        <h1>
+                            <div className="main_section_logo">
+                                <img className="logo1" src={require("../image/logo.png")} alt=""/>
+                                <h2 className="text">Consider design is a toll to get clients feedback.</h2>
+                                <p className="text"> Makes your process fast.</p>
+                            </div>
+                        </h1>
+                    </div>
+                </div>
+                <div className="section2">
+                    <div className="form_inline">
+                        <h2> Sign in to design</h2>
+                            <div className="signin_form">
+                                <input
+                                    className="form-control"
+                                    type="text"
+                                    placeholder="username"
+                                    onChange={event => this.setState({username: event.target.value})}
+                                /> <br/> <br/>
+                                <input
+                                    className="form-control"
+                                    type="password"
+                                    placeholder="password"
+                                    onChange={ event => this.setState({password: event.target.value})}
+                                /> <br/> <br/>
+                                <button
+                                    className="form-control"
+                                    type="button"
+                                    onClick={() => this.SignIn()}
+                                >
+                                    Login
+                                </button>
+                            </div>
                         </div>
-                    </h1>
-                </div>
-            <div className="form_inline">
-                <h2> Sign in to design</h2>
-                <div className="form_group">
-                    <input
-                        className="form_control"
-                        type="text"
-                        placeholder="username"
-                        onChange={event => this.setState({username: event.target.value})}
-                    />
-                    <input
-                        className="form_control"
-                        type="password"
-                        placeholder="password"
-                        onChange={ event => this.setState({password: event.target.value})}
-                    />
-                    <button
-                        className="from_control"
-                        type="button"
-                        onClick={() => this.SignIn()}
-                    >
-                        Login
-                    </button>
-
-                </div>
-
-            </div>
+                    </div>
             </div>
         );
     }
